2f7fb75833c8dadcad92bc9db184d1e08e8f9471
mr-manojk <>
2026-02-21 10:17:41 +0530

    Hotsing error

--- app.js
+++ app.js
@@ -8,14 +8,6 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-// Request Logging Middleware (Helpful for Render logs)
-app.use((req, res, next) => {
-  if (!req.url.startsWith('/uploads')) {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  }
-  next();
-});
-
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
