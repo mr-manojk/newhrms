b7bf145607c01b7a0d8b0e5b7e5e4a8ae5b2d37a
mr-manojk <>
2026-02-28 15:04:26 +0530

    Timeout defined in the mailService

--- services/mailService.js
+++ services/mailService.js
@@ -10,6 +10,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,  // 10s
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 const mailService = {
